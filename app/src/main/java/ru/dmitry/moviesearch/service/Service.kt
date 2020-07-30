199246d91515d3557997e383812656fda5c3f098
dapodshivalov <33161281+dapodshivalov@users.noreply.github.com>
2020-07-30 21:33:00 +0300

    init Service.kt

---
+++ app/src/main/java/ru/dmitry/moviesearch/service/Service.kt
@@ -0,0 +1,20 @@
+package ru.dmitry.moviesearch.service
+
+import android.telecom.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface Service {
+//    @GET("movie/popular")
+//    fun getPopularMovies(
+//        @Query("api_key") apiKey: String?,
+//        @Query("page") pageIndex: Int
+//    ): Call<MoviesResponse?>?
+//
+//    @GET("movie/top_rated")
+//    fun getTopRatedMovies(
+//        @Query("api_key") apiKey: String?,
+//        @Query("page") pageIndex: Int
+//    ): Call<MoviesResponse?>?
+}
